5756b7f943ea05def81405a0855d8c81fa2b5cdc
Rashmika <rashmikafdo20@gmail.com>
2025-04-03 22:37:58 +0530

    apps

---
+++ predict_page.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+def load_model():
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+    data = load_model()
+
+    regressor_loaded = data["model"]
+    le_country = data["le_country"]
+    le_education = data["le_education"]
+
+    def show_predict_page():
+        st.title("Software Developer Salary Prediction")
+
+        st.write("""### We need some information to predict the salary""")
