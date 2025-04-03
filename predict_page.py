40f4aab219c3d676b8fdefef19adc42d87391a39
Rashmika <rashmikafdo20@gmail.com>
2025-04-04 01:03:40 +0530

    final

--- predict_page.py
+++ predict_page.py
@@ -7,13 +7,56 @@ def load_model():
         data = pickle.load(file)
     return data
 
-    data = load_model()
 
-    regressor_loaded = data["model"]
-    le_country = data["le_country"]
-    le_education = data["le_education"]
+data = load_model()
 
-    def show_predict_page():
-        st.title("Software Developer Salary Prediction")
+regressor_loaded = data["model"]
+le_country = data["le_country"]
+le_education = data["le_education"]
 
-        st.write("""### We need some information to predict the salary""")
+def show_predict_page():
+    st.title("Software Developer Salary Prediction")
+
+    st.write("""### We need some information to predict the salary""")
+
+    countries = (
+        "US",
+        "India",
+        "UK",
+        "Germany",
+        "Canada",
+        "Brazil",
+        "France",
+        "Spain",
+        "Australia",
+        "Netherlands",
+        "Poland",
+        "Italy",
+        "Russian Federation",
+        "Sweden",
+    )
+
+    education = (
+        "Less than a Bachelors",
+        "Bachelor's degree",
+        "Master's degree",
+        "Post grad",
+
+    )
+
+    country = st.selectbox("Country", countries)
+    education= st.selectbox("Education Level", education)
+
+    experience = st.slider("Years of Experience",0,50,3 )
+
+    ok = st.button("Calculate Salary")
+    if ok:
+        X = np.array([[country, education, experience]])
+        X[:, 0] = le_country.transform(X[:,0])
+        X[:, 1] = le_education.transform(X[:,1])
+        X=X.astype(float)
+
+        salary = regressor_loaded.predict(X)
+        st.subheader(f"The estimated salary is ${salary[0]:.2f}")
+
+
